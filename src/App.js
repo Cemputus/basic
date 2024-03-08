82c970626f8d60c246d6f70d8fcfc434468b8118
Emmanuel Nsubuga <cemputus019@gmail.com>
2024-03-08 19:26:24 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 
+// // import React from 'react';
+// // import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// // import AttendanceTracker from './AttendanceTracker';
+// // import Navbar from './components/Navbar';
+
+// // function App() {
+// //   return (
+// //     <Router>
+// //       <div>
+// //         <Navbar />
+// //         <Route path="/" exact component={Home} />
+// //         <Route path="/attendance" component={AttendanceTracker} />
+// //       </div>
+// //     </Router>
+// //   );
+// // }
+
+// // function Home() {
+// //   return <h2>Home Page</h2>;
+// // }
+
+// // export default App;
+
+
+// // App.js
 // import React from 'react';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import AttendanceTracker from './AttendanceTracker';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import AttendanceTracker from './components/AttendanceTracker';
 // import Navbar from './components/Navbar';
 
 // function App() {
@@ -21,28 +46,28 @@
 // }
 
 // export default App;
-
-
-// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import AttendanceTracker from './components/AttendanceTracker';
 import Navbar from './components/Navbar';
 
 function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Route path="/" exact component={Home} />
-        <Route path="/attendance" component={AttendanceTracker} />
-      </div>
-    </Router>
+    <div>
+      <Navbar />
+      <AttendanceTracker />
+    </div>
   );
-}
+};
 
-function Home() {
-  return <h2>Home Page</h2>;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 }
 
 export default App;
